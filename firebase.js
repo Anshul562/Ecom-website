f511e8ee8da391e0b7c8978fea02d9b1aab6fed8
Anshul Gurjar <74780683+Anshul562@users.noreply.github.com>
2022-02-24 13:39:56 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC89NOOhCZJGmlXlH-55E9DZD6JzDTsyDI",
+    authDomain: "ecom-2d24c.firebaseapp.com",
+    projectId: "ecom-2d24c",
+    storageBucket: "ecom-2d24c.appspot.com",
+    messagingSenderId: "911020750141",
+    appId: "1:911020750141:web:176f8e397322ef86e18154"
+  };
